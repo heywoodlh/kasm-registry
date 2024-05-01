5425b0027cbcea1e25c68e95b654dec012326c14
Spencer Heywood <github@heywoodlh.io>
2024-05-01 13:50:17 -0600

    added debian-xfce workspace

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'heywoodlh kasm registry',
+    description: 'kasm registry by heywoodlh, for fun',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://kasmregistry.heywoodlh.io/',
+    contactUrl: 'https://heywoodlh.io/contact',
   },
   reactStrictMode: true,
   swcMinify: true,
